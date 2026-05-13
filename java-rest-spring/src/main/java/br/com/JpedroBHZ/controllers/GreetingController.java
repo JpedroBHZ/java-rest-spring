dafd5a2c9f4347b4a8419057b0f4f42acf0936af
JpedroBHZ <jpedro.bergamasco@gmail.com>
2026-05-13 08:55:06 -0300

    path-params-and-exceptions

--- java-rest-spring/src/main/java/br/com/JpedroBHZ/controllers/GreetingController.java
+++ java-rest-spring/src/main/java/br/com/JpedroBHZ/controllers/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    //http://localhost:8080/greeting/nome=João
+    //http://localhost:8080/greeting?name=João
     @RequestMapping("/greeting")
     public Greeting greeting(
             @RequestParam(value = "name", defaultValue = "word")
